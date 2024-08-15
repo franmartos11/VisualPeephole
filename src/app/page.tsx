da894fcb93987590f03044f735ac883f55ad23d5
franmartos11 <franmartos11@gmail.com>
2024-08-15 23:02:15 +0200

    comming soon update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,13 @@ import Image from "next/image";
 import { TabletHome } from "./Components/TabletHome/TabletHome";
 import { GridLayout } from "./Components/GridLayout/GridLayout";
 import { CardHoverEffectDemo } from "./Components/ServicesDisplay/CardHoverEffectDemo";
+import CommingSoon from "./Components/ComingSoon/CommingSoon";
 
-
+/*className="flex min-h-screen flex-col items-center justify-between "*/
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
-      <TabletHome></TabletHome>
-      <GridLayout></GridLayout>
-      <CardHoverEffectDemo></CardHoverEffectDemo>
+    <main >
+      <CommingSoon></CommingSoon>
     </main>
   );
 }
