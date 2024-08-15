9d9d9dee66137075faa0442750df6af61179355e
franmartos11 <franmartos11@gmail.com>
2024-08-15 23:15:32 +0200

    fix comming soon3

--- src/app/Components/ComingSoon/CommingSoon.tsx
+++ src/app/Components/ComingSoon/CommingSoon.tsx
@@ -4,7 +4,7 @@ export default function CommingSoon() {
             
             <h1 className="text-5xl md:text-7xl text-white font-bold mb-8 z-10"> Visual Peephole Coming Soon</h1>
             <p className="text-white text-xl md:text-2xl">
-                We're working hard to bring you something amazing. Stay tuned!
+                We re working hard to bring you something amazing. Stay tuned!
             </p>
         </div>
     )
