dbf21f645660c7c04308cd09f3326d1399af6f36
franmartos11 <franmartos11@gmail.com>
2024-08-15 23:03:43 +0200

    comming soon update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { GridLayout } from "./Components/GridLayout/GridLayout";
 import { CardHoverEffectDemo } from "./Components/ServicesDisplay/CardHoverEffectDemo";
 import CommingSoon from "./Components/ComingSoon/CommingSoon";
 
-/*className="flex min-h-screen flex-col items-center justify-between "*/
+
 export default function Home() {
   return (
     <main >
