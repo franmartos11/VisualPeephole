879fcb1286f3b982fc6d3b0e9fd3adb6bf7ee0d0
franmartos11 <franmartos11@gmail.com>
2024-08-16 18:19:19 -0300

    development2

--- src/app/Components/Globe/SkeletonFour.tsx
+++ src/app/Components/Globe/SkeletonFour.tsx
@@ -1,28 +1,32 @@
 "use client";
-import React from "react";
-import { cn } from "@/utils/cn";
+import React, { useEffect, useRef } from "react";
 import createGlob from "cobe";
-import { useEffect, useRef } from "react";
- 
 
- 
 export const SkeletonFour = () => {
   return (
-    <div className="h-60 md:h-60  flex flex-col items-center  mt-10">
-       <h3 className=" px-40 pb-10 text-4xl font-semibold text-white  text-center">Our Services</h3>
+    <div className="h-60 md:h-60 flex flex-col items-center mt-10">
+      <h3 className="px-40 pb-10 text-4xl font-semibold text-white text-center">
+        We work worldwide
+      </h3>
       <Globe className="" />
     </div>
   );
 };
- 
+
+interface GlobeState {
+  phi?: number;
+  theta?: number;
+  [key: string]: any; // Add other properties as needed
+}
+
 export const Globe = ({ className }: { className?: string }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
- 
+
   useEffect(() => {
     let phi = 0;
- 
+
     if (!canvasRef.current) return;
- 
+
     const globe = createGlob(canvasRef.current, {
       devicePixelRatio: 2,
       width: 600 * 2,
@@ -37,23 +41,22 @@ export const Globe = ({ className }: { className?: string }) => {
       markerColor: [0.1, 0.8, 1],
       glowColor: [1, 1, 1],
       markers: [
-        // longitude latitude
         { location: [37.7595, -122.4367], size: 0.03 },
         { location: [40.7128, -74.006], size: 0.1 },
       ],
-      onRender: (state: phi) => {
+      onRender: (state: GlobeState) => {
         // Called on every animation frame.
         // `state` will be an empty object, return updated params.
         state.phi = phi;
         phi += 0.01;
       },
     });
- 
+
     return () => {
       globe.destroy();
     };
   }, []);
- 
+
   return (
     <canvas
       ref={canvasRef}
@@ -62,3 +65,4 @@ export const Globe = ({ className }: { className?: string }) => {
     />
   );
 };
+
