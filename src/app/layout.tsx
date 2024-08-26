7c516b68e664276c5493a0b5fec9a97e308f4a6f
franmartos11 <franmartos11@gmail.com>
2024-08-26 15:36:45 -0300

    add google analitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { NavbarDemo } from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <Script
+                    id="gtm"
+                    strategy="afterInteractive" 
+                    dangerouslySetInnerHTML={{
+                    __html: ` (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KZFPM2ML'); ` }}
+                ></Script>
+
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
