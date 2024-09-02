93488d05a25e9204e0663ab0780c015ccda3a00c
franmartos11 <franmartos11@gmail.com>
2024-09-02 17:42:20 -0300

    fix aboutusteam

--- src/app/Components/AboutUsTeam/AboutUsTeam.tsx
+++ src/app/Components/AboutUsTeam/AboutUsTeam.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 
-export default function () {
+export default function AboutUsTeam() {
     const [hovered, setHovered] = useState(false);
     const [hovered1, setHovered1] = useState(false);
     const [hovered2, setHovered2] = useState(false);
