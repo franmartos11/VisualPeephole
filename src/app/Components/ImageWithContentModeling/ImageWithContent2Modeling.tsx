c8b376e4411d91851a957b11b0c3301b91f67eb3
franmartos11 <franmartos11@gmail.com>
2024-09-06 10:47:24 -0300

    add components to Modeling page

---
+++ src/app/Components/ImageWithContentModeling/ImageWithContent2Modeling.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import Image from "next/image";
+export default function ImageWithContent2Modeling() {
+    return (
+        <section>
+            <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8">
+                <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+                    <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:h-full">
+                        
+                        <img
+                        className="w-full h-full object-cover rounded-lg "
+                        src="/modeling.jpg"
+                        alt="can_help_banner"
+                    />
+                    </div>
+                    <div className="lg:py-24">
+                        <h2 className="text-4xl text-black font-bold ">
+                        Turn Your Vision into Reality
+                        </h2>
+                        <p className="mt-4 text-base text-gray-800">
+                        Transform your projects with our high-definition 3D object modeling services. Our team excels in creating detailed and photorealistic 3D models that bring your concepts to life. Whether you need product prototypes, architectural elements, or custom designs, our precision-crafted models ensure accuracy and realism. Enhance engagement with interactive 3D designs optimized for web performance. Trust our tailored solutions to meet the unique needs of every industry, ensuring timely delivery and exceptional quality.</p>
+                        <div className="mt-12 mb-10 text-center">
+                            <Link
+                                title="link"
+                                href="/contact"
+                                className="inline-block rounded bg-gray-500 px-[4rem] py-3 text-sm font-medium text-white transition hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-400"
+                            >
+                                Contact Us
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
--- src/app/Modeling/page.tsx
+++ src/app/Modeling/page.tsx
@@ -1,5 +1,9 @@
 import FeatureModeling from "../Components/FeatureModeling/FeatureModeling";
+import Form from "../Components/Forms/Form";
+import ImageWithContent2Modeling from "../Components/ImageWithContentModeling/ImageWithContent2Modeling";
+import ImageWithContentModeling from "../Components/ImageWithContentModeling/ImageWithContentModeling";
 import ImgsExplicationModeling from "../Components/ImgsExplicationModeling/ImgsExplicationModeling";
+import Promo from "../Components/Promo/Promo";
 import { TextModeling } from "../Components/TextModeling/TextModeling";
 
 export default function Modeling(){
@@ -8,7 +12,10 @@ export default function Modeling(){
             <TextModeling></TextModeling>
             <ImgsExplicationModeling></ImgsExplicationModeling>
             <FeatureModeling></FeatureModeling>
-
+            <ImageWithContent2Modeling></ImageWithContent2Modeling>
+            <Promo></Promo>
+            <ImageWithContentModeling></ImageWithContentModeling>
+            <Form></Form>
         </div>
     )
 }
